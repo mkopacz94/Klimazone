66097e2a24dd24eadc95fc48ab6887db207010e1
mkopacz94 <m.kopacz94@gmail.com>
2021-07-01 19:19:50 +0200

    Created navbar and forecast component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ForecastModule } from './forecast/forecast.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ForecastComponent } from './forecast/components/forecast/forecast.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ForecastModule
+    ForecastModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/forecast/forecast.module.ts
+++ src/app/forecast/forecast.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { ForecastComponent } from './components/forecast/forecast.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ForecastComponent
+  ],
   imports: [
     CommonModule,
     HttpClientModule
+  ],
+  exports: [
+    ForecastComponent
   ]
 })
 export class ForecastModule { }
